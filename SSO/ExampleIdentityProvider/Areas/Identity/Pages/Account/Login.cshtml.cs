d31703af8622c6b1fb63bb2f3b293a787fefaaf5
Teguh Subarkah <mteguhsubarkah@gmail.com>
2021-04-18 12:50:46 +0700

    initial commit by Teguh

--- SSO/ExampleIdentityProvider/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ SSO/ExampleIdentityProvider/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-
+// cobain gan
 namespace ExampleIdentityProvider.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
